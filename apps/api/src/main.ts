2361ac83c57b642550ab3ed2d596f92111cc2108
michaelkyng <michaelakpuofoba@gmail.com>
2026-06-03 23:22:36 +0100

    fix : added fallback port

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
 
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, documentFactory());
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   console.log(`API is running on port ${process.env.PORT ?? 3000}`);
 }
 void bootstrap();
